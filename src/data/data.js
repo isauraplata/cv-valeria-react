dbab68e2278bd2cecca9462b786d5cc78f91b843
valie <isauravpr@gmail.com>
2023-01-21 15:19:00 -0600

    finished

--- src/data/data.js
+++ src/data/data.js
@@ -2,15 +2,15 @@ export let data = {
   name: "valeria",
   lastName: "plata",
   level: "Junior Developer",
-  contact: ["isauraplatarojas@gmail.com", "mexico", "96138658"],
-  schools: ["Colegio Gilberto Velazquez", "Universidad Politecnica de Chiapas"],
-  skills: ["Javascript", "Java", "Nodejs", "React"],
+  contact: ["isauraplatarojas@gmail.com", "mexico", "96138658","@valeria_plata"],
+  schools: ["Colegio Gilberto Velazquez Norte","Colegio Gilberto Velazquez Sur", "Universidad Politecnica de Chiapas"],
+  skills: ["Javascript", "Java", "Nodejs", "React", "Html5"],
   experience: [
-    "Aplicacion web con nodejs (Express) y sql, tienda en linea con carrito de compra y seguimiento de envios",
-    "Aplicacion Escritorio con Java, usando JavaFx y SceneBuilder, aplicacion de recordatorio de tareas usando estructuras de datos",
-    "C++ porfe diana",
+    "Aplicacion web con nodejs (Express) y sql, tienda en linea con carrito de compra, apartado de comentarios y seguimiento de envios",
+    "Aplicacion Escritorio con Java, usando JavaFx y SceneBuilder, aplicacion de recordatorio de tareas usando estructuras de datos (ArrayList)",
+    "C++ Aplicacion por consola de busqueda de servicios aleatoriamente",
   ],
   lenguajes: ["Spanish", "English"],
   description:
-    " I am a software engineering student, I like to learn new things, I have a interesent in ",
+    " I am a software engineering student, I like to learn new things, I am interested in growing professionally",
 };
